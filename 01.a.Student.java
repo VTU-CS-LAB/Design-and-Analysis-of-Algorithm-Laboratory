a257a7b7c09bcd26fe91c72d98effdb98a9588f2
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-19 14:53:55 +0530

    Initial Commit

---
+++ 01.a.Student.java
@@ -0,0 +1,67 @@
+
+/*
+1.a. Create a Java class called Studentwith the following details as variables within it.
+(i) USN
+(ii) Name
+(iii) Branch
+(iv) Phone
+Write a Java program to create nStudent objects and print the USN, Name, Branch, and
+Phoneof these objects with suitable headings. 
+*/
+
+import java.util.Scanner;
+
+class Student {
+    private String usn, name, branch, ph;
+
+    public Student() {
+    }
+
+    public void accept() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Usn\n");
+        usn = scanner.next();
+
+        System.out.println("Enter Name\n");
+        name = scanner.next();
+
+        System.out.println("Enter Branch\n");
+        branch = scanner.next();
+
+        System.out.println("Enter Phone\n");
+        ph = scanner.next();
+
+        scanner.close();
+    }
+
+    public void display() {
+        System.out.println("Student details");
+        System.out.println("Usn: " + usn);
+        System.out.println("Name: " + name);
+        System.out.println("Branch: " + branch);
+        System.out.println("Phone: " + ph);
+        System.out.println();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value for n");
+        int n = scanner.nextInt();
+        Student[] ob1 = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            ob1[i] = new Student();
+            ob1[i].accept();
+        }
+
+        for (int i = 0; i < n; i++) {
+            ob1[i].display();
+        }
+
+        scanner.close();
+    }
+}
---
+++ 01.b.Stack.java
@@ -0,0 +1,83 @@
+
+/*
+1.b. Write a Java program to implement the Stack using arrays. Write Push(), Pop(), and
+Display() methods to demonstrate its working.
+*/
+
+import java.util.Scanner;
+
+class Stack {
+    private int arr[], top, size;
+
+    public Stack(int size) {
+        this.size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    public void push(int i) {
+        if (top == size - 1) {
+            System.out.println("Stack is full");
+            return;
+        }
+        arr[++top] = i;
+    }
+
+    public void pop() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.println("Element Popped is: " + arr[top--]);
+    }
+
+    public void display() {
+        if (top == -1) {
+            System.out.print("Empty\n");
+            return;
+        }
+        System.out.print("\nStack: ");
+        for (int i = top; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Size of Integer Stack ");
+        int n = scanner.nextInt();
+
+        Stack stack = new Stack(n);
+
+        WhileLoop: while (true) {
+            System.out.println("\nStack Operations");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Display");
+            System.out.println("4. Exit");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+            case 1:
+                System.out.println("Enter integer element to push");
+                stack.push(scanner.nextInt());
+                break;
+
+            case 2:
+                stack.pop();
+                break;
+
+            case 3:
+                stack.display();
+                break;
+
+            case 4:
+                break WhileLoop;
+            }
+        }
+        scanner.close();
+    }
+}
