c3feac70857723ddcadeadc848eb72011c596720
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-05-14 20:10:02 +0530

    path displayed

--- 10.b.TravellingPerson.java
+++ 10.b.TravellingPerson.java
@@ -8,13 +8,18 @@ import java.util.Scanner;
 
 class Main {
     static int[][] graph;
+    static int[] path, tempPath;
+    static int n;
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number of cities");
-        int n = scanner.nextInt();
+        n = scanner.nextInt();
 
+        path = new int[n];
+        tempPath = new int[n];
         graph = new int[n][n];
+
         System.out.println("Enter Adjacency Matrix");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -36,21 +41,39 @@ class Main {
         int cost = tsp(src - 1, set);
         System.out.println("Total Cost: " + cost);
 
+        System.out.print("Path: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print((path[i] + 1) + " ");
+        }
+        System.out.println();
     }
 
     static int tsp(int v, ArrayList<Integer> set) {
         if (set.isEmpty()) {
             return graph[v][0];
         }
-
+        int size = set.size();
         ArrayList<Integer> subSet;
         int minCost = Integer.MAX_VALUE;
         for (Integer i : set) {
             subSet = new ArrayList<Integer>(set);
             subSet.remove(i);
             int cost = graph[v][i] + tsp(i, subSet);
-            minCost = Math.min(minCost, cost);
+            // minCost = Math.min(minCost, cost);
+            if (cost < minCost) {
+                minCost = cost;
+                tempPath[size - 1] = i;
+                if (size == n - 1) {
+                    copyArray(path, tempPath);
+                }
+            }
         }
         return minCost;
+    }
+
+    static void copyArray(int[] dest, int[] src) {
+        for (int i = 0; i < n; i++) {
+            dest[i] = src[i];
+        }
     }
 }
